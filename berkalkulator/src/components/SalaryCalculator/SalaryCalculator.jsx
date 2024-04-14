a322ef9ecb8852f3987e2dc6ea384da5efb62507
carburator03 <carburator03@proton.me>
2024-04-14 21:39:21 +0200

    added labels, slider updates value in the input field

--- berkalkulator/src/components/SalaryCalculator/SalaryCalculator.jsx
+++ berkalkulator/src/components/SalaryCalculator/SalaryCalculator.jsx
@@ -1,5 +1,6 @@
 import { Input } from '@/components/ui/input';
 import { Slider } from '../ui/slider';
+import { Label } from '@radix-ui/react-label';
 
 const SalaryCalculator = ({ member, members, setMembers, index }) => {
   const handleNameChange = e => {
@@ -8,30 +9,43 @@ const SalaryCalculator = ({ member, members, setMembers, index }) => {
   };
 
   const handleEarningChange = e => {
-    member.earning = e.target.value;
+    const earning = parseInt(e.target.value);
+    if (!isNaN(earning) && earning >= 10000 && earning <= 2000000) {
+      member.earning = earning;
+      setMembers([...members.slice(0, index), member, ...members.slice(index + 1)]);
+    }
+  };
+
+  const handleEarningChangeSlider = value => {
+    member.earning = value[0];
     setMembers([...members.slice(0, index), member, ...members.slice(index + 1)]);
+    document.getElementById('earning').value = value[0];
   };
 
   return (
     <div className='bg-pink-400 p-5'>
+      <Label htmlFor='name'>Családtag neve</Label>
       <Input
         type='text'
         placeholder='Feri'
         defaultValue={member.name}
         onChange={handleNameChange}
+        id='name'
       />
+      <Label htmlFor='earning'>Fizetése</Label>
       <Input
         type='text'
         placeholder='250000'
         defaultValue={member.earning}
         onChange={handleEarningChange}
+        id='earning'
       />
       <Slider
-        defaultValue={[250000]}
+        defaultValue={[member.earning]}
         max={2000000}
         min={10000}
         step={1000}
-        onValueChange={value => console.log(value)}
+        onValueChange={handleEarningChangeSlider}
       />
     </div>
   );
