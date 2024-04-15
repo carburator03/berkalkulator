20fcbaba3df5940951be0e49e7701fbabddaf2e9
carburator03 <carburator03@proton.me>
2024-04-16 01:07:42 +0200

    added date picker
    and we can pick a date and store in a usestate

--- berkalkulator/src/components/MarriedDateDialog/MarriedDateDialog.jsx
+++ berkalkulator/src/components/MarriedDateDialog/MarriedDateDialog.jsx
@@ -1,5 +1,3 @@
-import { Copy } from 'lucide-react';
-
 import { Button } from '@/components/ui/button';
 import {
   Dialog,
@@ -11,32 +9,33 @@ import {
   DialogTitle,
   DialogTrigger,
 } from '@/components/ui/dialog';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
 import { Badge } from '@/components/ui/badge';
+import { DatePicker } from './DatePicker';
+import { useState, useEffect } from 'react';
 
-export function MarriedDateDialog() {
+export function MarriedDateDialog({ marriedBenefit, setMarriedBenefit }) {
+  const [date, setDate] = useState();
+
+  useEffect(() => {
+    console.log(date, '- Has changed');
+  }, [date]);
+
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <Badge className='cursor-pointer'>Badge</Badge>
+        <Badge className='cursor-pointer'>Dátum</Badge>
       </DialogTrigger>
       <DialogContent className='sm:max-w-md'>
         <DialogHeader>
-          <DialogTitle>Share link</DialogTitle>
-          <DialogDescription>Anyone who has this link will be able to view this.</DialogDescription>
+          <DialogTitle>Házasságkötés dátuma</DialogTitle>
+          <DialogDescription>
+            A kedvezmény először a házasságkötést követő hónapra vehető igénybe és a házassági
+            életközösség alatt legfeljebb 24 hónapon keresztül jár.
+          </DialogDescription>
         </DialogHeader>
-        <div className='flex items-center space-x-2'>
-          <div className='grid flex-1 gap-2'>
-            <Label htmlFor='link' className='sr-only'>
-              Link
-            </Label>
-            <Input id='link' defaultValue='https://ui.shadcn.com/docs/installation' readOnly />
-          </div>
-          <Button type='submit' size='sm' className='px-3'>
-            <span className='sr-only'>Copy</span>
-            <Copy className='h-4 w-4' />
-          </Button>
+        <div className=''>
+          {' '}
+          <DatePicker date={date} setDate={setDate} />
         </div>
         <DialogFooter className='sm:justify-start'>
           <DialogClose asChild>
