8a70285d1eaea2559516e868467850b06a64008f
carburator03 <carburator03@proton.me>
2024-04-19 11:54:49 +0200

    added localstorage support
    date and marriedBenefit needs to be saved too

--- berkalkulator/src/components/HouseholdSalaryCalculator.jsx
+++ berkalkulator/src/components/HouseholdSalaryCalculator.jsx
@@ -1,34 +1,47 @@
 import FamilyMemberTabs from './FamilyMemberTabs/FamilyMemberTabs';
 import HouseholdSummary from './HouseholdSummary/HouseholdSummary';
-import { Button } from '@/components/ui/button';
-import { Plus } from 'lucide-react';
-import { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const HouseholdSalaryCalculator = () => {
-  const [members, setMembers] = useState([
-    {
-      name: 'Peti',
-      salary: 100000,
-      young: false,
-      married: false,
-      personnel: false,
-      family: true,
-      dependent: 1,
-      dependent2: 0,
-      net: 0,
-    },
-    {
-      name: 'Jani',
-      salary: '',
-      young: false,
-      married: true,
-      personnel: false,
-      family: true,
-      dependent: 2,
-      dependent2: 1,
-      net: 0,
-    },
-  ]);
+  const [members, setMembers] = useState(() => {
+    const data = localStorage.getItem('members');
+    return data
+      ? JSON.parse(data)
+      : [
+          {
+            name: 'Feri',
+            salary: 600000,
+            young: true,
+            married: false,
+            personnel: false,
+            family: true,
+            dependent: 1,
+            dependent2: 0,
+            net: 0,
+          },
+          {
+            name: 'Jani',
+            salary: '550000',
+            young: false,
+            married: true,
+            personnel: false,
+            family: true,
+            dependent: 2,
+            dependent2: 1,
+            net: 0,
+          },
+        ];
+  });
+
+  useEffect(() => {
+    const data = localStorage.getItem('members');
+    if (data) setMembers(JSON.parse(data));
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('members', JSON.stringify(members));
+  }, [members]);
+
   return (
     <>
       <div className='flex justify-center w-5/6 m-auto gap-3'>
